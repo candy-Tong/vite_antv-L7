4f5d9215fdbeeea993982e4d824a9a3832b4bbb1
lc06 <55652170+lc06@users.noreply.github.com>
2021-08-05 12:54:22 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,31 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import cesium from "vite-plugin-cesium";
+import { defineConfig } from "vite"
+import swc from "rollup-plugin-swc"
+import vue from "@vitejs/plugin-vue"
+import cesium from "vite-plugin-cesium"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), cesium()],
+  plugins: [
+    vue(),
+    cesium(),
+    swc({
+      jsc: {
+        parser: {
+          syntax: "typescript",
+          //tsx: true,//if you use react
+          dynamicImport: true,
+          decorators: true,
+        },
+        target: "es2021",
+        transform: {
+          decoratorMetadata: true,
+        },
+      },
+    }),
+  ],
+  esbuild: false,
   server: {
-    port: 8081
-  }
-});
+    port: 8081,
+  },
+})
+
